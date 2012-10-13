430ea96d41d250b17ff9fb8df4dd5bc92dfd9e56
Rakunho <rakatongo@hotmail.com>
2012-10-13 08:18:55 -0300

    Terminado layout y rutas

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,36 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
-	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	subject { page }	
 
   describe "Home" do
-  	it "should have el contentido 'Sample App' " do
-  		visit root_path
-  		page.should have_selector('h1',text: "Sample App")
-  	end
-  	it "should have the right title" do
-	  visit root_path
-	  page.should have_selector('title',
-	                    :text => "#{base_title}")
-	end
+  	before { visit root_path }  	
+
+  	it {should have_selector('h1',text: "Sample App")}  	
+  	it {should have_selector('title', :text => titulo(''))}  	
+  	it {should_not have_selector('title', :text => titulo('Home'))}  	
 
   end
+
   describe 'Help' do
-  	it "should have el contenido 'Help'" do
-  		visit help_path
-  		page.should have_selector('h1',text:"Help")
-  	end
-  	it "should have the right title" do
-	  visit help_path
-	  page.should have_selector('title',
-	                    :text => "#{base_title} | Help")
-	end
+  	before{ visit help_path }
+
+  	it {should have_selector('h1', text: "Help")}
+  	it {should have_selector('title', :text => titulo('Help'))}  	
   end
+
   describe 'About' do
-  	it "should have el contenido 'About us' " do
-  		visit about_path
-  		page.should have_selector('h1',text:"About Us")
-  	end
-  	it "should have the right title" do
-	  visit about_path
-	  page.should have_selector('title',
-	                    :text => "#{base_title} | About Us")
-	end
+  	before {visit about_path }  	
+
+  	  it {page.should have_selector('h1',text:"About Us")}	
+	  it {page.should have_selector('title',:text => titulo('About Us'))}	
   end
 
   describe "Contact page" do
+  	before { visit contact_path }
 
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title', :text => "#{base_title} | Contact")
-    end
+      it {page.should have_selector('h1', :text => 'Contact')}      
+      it {page.should have_selector('title', :text => titulo('Contact'))}
+    
   end
 end
